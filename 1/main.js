d56c8ee007adcabfe0b165f78ea90a1488e24ecd
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-09-21 22:24:46 +0200

    first site

--- 1/main.js
+++ 1/main.js
@@ -1,8 +1,17 @@
 // console.log(dbl)
 let accountsTableBody = document.querySelector("#accounts-table-body");
+let addAccountsViewBtn = document.querySelector('[href="account-view"]');
+// console.log(accountsViewBtn)
+let accountsViewBtn = document.querySelector('[href="add-account-view"]');
+let accountsView = querySelector("#account-view");
+let addAccountsView = querySelector("#add-account-view");
 
+addAccountsViewBtn.addEventListener("click", function (e) {
+  e.preventDefault;
+  addAccountsView.style.display = "block";
+  accountsView.style.display = "none";
+});
 
-+createAccountsTable();
 function createAccountsTable() {
   let htmlAccount = "";
   for (let i = 0; i < dbl.length; i++) {
@@ -18,3 +27,4 @@ function createAccountsTable() {
   console.log(htmlAccount);
   accountsTableBody.innerHTML = htmlAccount;
 }
+createAccountsTable();
