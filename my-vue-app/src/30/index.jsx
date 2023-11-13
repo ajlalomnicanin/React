de1c6771db39ab1c2730ff79346b6cf1c5c4b98d
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2023-11-13 20:57:02 +0100

    Class25

---
+++ my-vue-app/src/30/index.jsx
@@ -0,0 +1,14 @@
+// import Fetch from "./Fetch";
+import Search from "./search";
+
+const Class25 = () => {
+  return (
+    <div>
+      <Search/>
+      {/* <Fetch /> */}
+    </div>
+  );
+};
+export default Class25;
+
+// kako menjamo kveri parametre fecanje
--- my-vue-app/src/main.jsx
+++ my-vue-app/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Class24 from "./29";
+import Class25 from "./30";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Class24 />
+    <Class25 />
   </React.StrictMode>
 );
